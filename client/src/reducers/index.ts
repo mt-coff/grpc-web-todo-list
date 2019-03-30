343418bce509ef510c861c630dd2d03c0073aabc
mt-coff <mt.coff.00@gmail.com>
2019-03-30 20:35:12 +0900

    fix return state method

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -18,23 +18,13 @@ export default (state = initialState, action: any) => {
     case REQUEST_ADD_TODO:
       return state;
     case RECEIVE_ADD_TODO:
-      return Object.assign({}, state, {
-        todo: action.todo,
-        isError: false,
-        errorMsg: ''
-      });
+      return { ...state, todo: action.todo, isError: false, errorMsg: '' };
     case REQUEST_TODO_LIST:
       return state;
     case RECEIVE_TODO_LIST:
-      return Object.assign({}, state, {
-        todoList: action.todoList.todoList,
-        isError: false
-      });
+      return { ...state, todoList: action.todoList.todoList, isError: false };
     case HANDLE_ERROR:
-      return Object.assign({}, state, {
-        isError: true,
-        errorMsg: action.msg
-      });
+      return { ...state, isError: true, errorMsg: action.msg };
     default:
       return state;
   }
